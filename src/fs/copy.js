1d1aef3d8b732fee05bd41f330804a8ae75ed283
Timur Lagawy <timurtagirovich@mail.ru>
2024-01-28 20:13:59 +0100

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 const copy = async () => {
-    // Write your code here 
+  const sourceFolderPath = path.join(__dirname, "files");
+  const destinationFolderPath = path.join(__dirname, "files_copy");
+
+  try {
+    await fs.access(destinationFolderPath);
+    throw new Error("FS operation failed: Destination folder already exists");
+  } catch (error) {
+    try {
+      await fs.mkdir(destinationFolderPath);
+
+      const files = await fs.readdir(sourceFolderPath);
+
+      for (const file of files) {
+        const sourceFilePath = path.join(sourceFolderPath, file);
+        const destinationFilePath = path.join(destinationFolderPath, file);
+        await fs.copyFile(sourceFilePath, destinationFilePath);
+      }
+
+      console.log("Files copied successfully to:", destinationFolderPath);
+    } catch (copyError) {
+      throw new Error(`FS operation failed: ${copyError.message}`);
+    }
+  }
 };
 
 await copy();
