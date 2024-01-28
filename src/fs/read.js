6d8f286694298de2a560ec59a069dd152f54bc4c
Timur Lagawy <timurtagirovich@mail.ru>
2024-01-28 20:38:46 +0100

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "fileToRead.txt");
+
+  try {
+    const content = await fs.readFile(filePath, "utf-8");
+
+    console.log("File content:", content);
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
 await read();
