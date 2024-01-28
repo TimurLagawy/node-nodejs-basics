0ddd7233b59b9e06bf4a58a576d9e9c8721b6a65
Timur Lagawy <timurtagirovich@mail.ru>
2024-01-28 20:31:46 +0100

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
-const remove = async () => {
-    // Write your code here 
+const fs = require("fs").promises;
+const path = require("path");
+
+const deleteFile = async () => {
+  const filePath = path.join(__dirname, "fileToRemove.txt");
+
+  try {
+    await fs.access(filePath);
+
+    await fs.unlink(filePath);
+    console.log("File deleted successfully:", filePath);
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
-await remove();
+await deleteFile();
