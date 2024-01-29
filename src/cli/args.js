4f80c62d799f66b496e4a3e7397b6b46f90448e8
Timur Lagawy <timurtagirovich@mail.ru>
2024-01-29 21:34:36 +0100

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const parsedArgs = {};
+
+  args.forEach((arg) => {
+    const [propName, value] = arg.split("=");
+    parsedArgs[propName.replace(/^-+/g, "")] = value;
+  });
+
+  console.log(parsedArgs);
 };
 
 parseArgs();
